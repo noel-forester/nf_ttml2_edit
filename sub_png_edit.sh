01a9baf149b45854a06507908d29df0c9cbdf7b8
noel-forester <62397558+noel-forester@users.noreply.github.com>
2021-07-14 08:41:25 +0900

    Update sub_png_edit.sh

--- sub_png_edit.sh
+++ sub_png_edit.sh
@@ -21,16 +21,19 @@ if [ ${WH} = ${extentxy} ] ; then
 	echo "video and xml resolution is match"
 else
 	echo "video and xml resolution is unmatch"
+	#縦横解像度差の倍率を計算
 	scalex=`echo "scale=5;${Width}/${extentx}*100" | bc`
 	scalex=`printf "%.0f" $scalex`
 	scaley=`echo "scale=5;${Height}/${extentxy}*100" | bc`
 	scaley=`printf "%.0f" $scaley`
+	#比率が小さいほうを判定
 	if [ ${scalex} -gt ${scaley} ] ; then
 		scaling=${scalex}
 	else
 		scaling=${scaley}
 	fi
-	echo ${scaling}
+	#echo ${scaling}
+	#pngの解像度を調整
 	for i in `seq 1 ${all}` ;do
 		printf "\r%s" "${i}/${all}"
 		convert "${i}.png" -geometry "${scaling}%" "${i}.png"
@@ -38,12 +41,14 @@ else
 	i=1
 fi
 
-
+#xmlを一行づつ読んで画像処理
 grep "<div begin" manifest_ttml2.xml  | sed 's/.*tts:extent=".*" .*"\([1-9].*\)".*/\1/'| sed 's/px//g' | while read line ; do
 
+#pngのサイズ取得
 px=`identify -format "%[width]" ${i}.png`
 py=`identify -format "%[height]" ${i}.png`
 
+#配置位置を設定
 ox=`echo "$line" | cut -d " " -f 1`
 ox=`echo "scale=5;${ox}/${extentx}*$Width" | bc`
 ox=`printf "%.0f" ${ox}`
@@ -52,6 +57,7 @@ oy=`echo "$line" | cut -d " " -f 2`
 oy=`echo "scale=5;${oy}/${extenty}*$Height" | bc`
 oy=`printf "%.0f" ${oy}`
 
+#上下左右の余白量を設定
 Ladd=$ox
 Tadd=$oy
 Radd=$(( $Width - $px - $ox ))
@@ -59,8 +65,10 @@ Badd=$(( $Height - $py - $oy ))
 
 #echo "Width $Width Height $Height px $px py $py ox $ox oy $oy Ladd $Ladd Tadd $Tadd Radd $Radd Badd $Badd"
 #echo "$(($Ladd+$Radd+$px)) $(($Tadd+$Badd+$py))"
+#進捗表示
 printf "\r%s" "${i}/${all}"
 
+#画像編集
 convert ${i}.png -background none -gravity southeast -splice ${Radd}x${Badd} ${i}.png
 convert ${i}.png -background none -gravity northwest -splice ${Ladd}x${Tadd} ${i}.png
 
